46ff4168888950eeb918cb1ccf47425b98ea2496
punit978 <punitprabhat9@gmail.com>
2024-12-24 00:01:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "🫵 Hey {first} 🥰\n\n<b>Plz Dono Chhannel ko join kre aur uske baad Try again pr click kre\n\nKindly Please join Channel 👇</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
@@ -58,7 +58,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "❗Aapko koi møvie chahiye to møvie channel ki links @hd99flix pr di gyi hai. waha se movie choose kre❗"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
